57fd84ea0781bfc61d3fd8728b9d3717720d347c
casino-man <ryan@7-77.eu>
2022-11-08 17:29:06 -0100

    x

--- config/casino-dog-operator-api.php
+++ config/casino-dog-operator-api.php
@@ -4,32 +4,19 @@
 return [
 
     'access' => [
-      'key' => '25ec095c710439e79f7eb254b93d0403',
-      'secret' => '551189d27',
+      'key' => env('WAINWRIGHT_CASINODOG_OPERATOR_KEY', '25ec095c710439e79f7eb254b93d0403'),
+      'secret' => env('WAINWRIGHT_CASINODOG_OPERATOR_SECRET', '123456'),
   ],
     'test_settings' => [
-        'start_balance' => 22500, // enter starting balance (integer in cents)
+        'start_balance' => env('WAINWRIGHT_CASINODOG_OPERATOR_STARTING_BALANCE', 22500), // enter starting balance (integer in cents)
     ],
-
-    /* Firewall is used within RestrictIpAddressMiddleware */
-    'firewall' => [
-        'https_only' => false, // redirect requests in http to https
-        'restrict_callback' => false,
-        'restrict_all_routes' => false, // restrict full app on ip
-        'allowed_ip' => [
-          '85.148.48.255',
-          '127.0.0.1'
-        ],
-      ],
 
     // You can customize endpoints as you like, and split/scale however you feel fit. 
     // Please do note if you change a single endpoint to go to different host that the "casino-dog-operator-api:connect-to-api" will not update custom endpoints
-    'api_url' => 'https://casinoman.app', /* api_url is the base url to contact, it should not end with slash */
+    'api_url' => env('WAINWRIGHT_CASINODOG_OPERATOR_API_BASEURL', 'https://casinoman.app'), /* api_url is the base url to contact, it should not end with slash */
     'endpoints' => [
-      'create_session' => 'https://casinoman.app/api/createSession',
-      'toggle_respin' => 'https://casinoman.app/api/control/toggle_respin',
-      'gameslist' => 'https://casinoman.app/api/gameslist/all',
-      'access_ping' => 'https://casinoman.app/api/accessPing',
+      'create_session' => env('WAINWRIGHT_CASINODOG_OPERATOR_API_CREATESESSION', 'https://casinoman.app/api/createSession'),
+      'gameslist' =>  env('WAINWRIGHT_CASINODOG_OPERATOR_API_GAMESLIST', 'https://casinoman.app/api/gameslist/all'),
+      'access_ping' => env('WAINWRIGHT_CASINODOG_OPERATOR_API_ACCESSPING', 'https://casinoman.app/api/accessPing'),
     ],
 ];
-
